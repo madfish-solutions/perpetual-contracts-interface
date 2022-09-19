1ae80cbe56c4b3106ce5d2a04b60505c02296e22
cyace84 <cyace84@gmail.com>
2022-09-19 15:40:52 +0200

    added abi

---
+++ abi/index.ts
@@ -0,0 +1,4 @@
+import clearingHouseAbi from "./ClearingHouse.json";
+import chainLinkAbi from "./ChainlinkPriceFeed.json";
+export const clearingHouseABI = clearingHouseAbi;
+export const chainLinkABI = chainLinkAbi;
